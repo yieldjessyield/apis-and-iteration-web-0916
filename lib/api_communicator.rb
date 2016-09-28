7a6e431a4230dafb8dcc1f2bc69787b1dc32d2d4
Jessica Harrelson <jess.harrelson0707@gmail.com>
2016-09-28 15:54:37 -0400

    made it work

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,26 +1,60 @@
 require 'rest-client'
+require 'JSON'
+require 'pry'
+
+def web_request(url)
+rest = RestClient.get(url)
+JSON.parse(rest)
+end
+
+#puts web_request("http://www.swapi.co/api/films/6/")
 
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  # iterate ove the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film. 
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list 
-  #  of movies by title. play around with puts out other info about a given film.
+  films_hash = []
+#character_hash["results"][0]["films"].each do |our_urls| films_hash << web_request(our_urls)
+
+#character_urls = character_data["films"]
+#character_urls.each do |url| films_hash << web_request(url)
+  character_urls = character_hash["results"].map do |character_data|
+    if character_data["name"].downcase == character
+    #binding.pry
+      films_hash = character_data["films"]
+  end
+    end
+  films_hash.collect do |url| web_request(url)
+#binding.pry
 end
 
+    # iterate over the character hash to find the collection of `films` for the given
+    #   `character`
+    # collect those film API urls, make a web request to each URL to get the info
+    #  for that film
+    # return value of this method should be collection of info about each film.
+    #  i.e. an array of hashes in which each hash reps a given film
+    # this collection will be the argument given to `parse_character_movies`
+    #  and that method will do some nice presentation stuff: puts out a list
+    #  of movies by title. play around with puts out other info about a given film.
+end
+
+#get_character_movies_from_api("luke skywalker")
+
 def parse_character_movies(films_hash)
+  #binding.pry
+  #films_hash[0]["title"].each |
+  films_hash.collect do |film|
+    puts film["title"]
+  end
   # some iteration magic and puts out the movies in a nice list
 end
 
+#parse_character_movies(films_hash)
+
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
+  #binding.pry
   parse_character_movies(films_hash)
 end
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,13 @@
+#require 'pry'
 def welcome
-  # puts out a welcome message here!
+  puts "HELLO SPACE TRAVERLER!!"
 end
 
 def get_character_from_user
   puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased. 
+  gets.chomp.downcase
+#binding.pry
+  # use gets to capture the user's input. This method should return that input, downcased.
 end
 
+#get_character_from_user
